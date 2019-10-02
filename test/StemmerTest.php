7f884e8d5f541489f9f9d69f30e6c34778a63e96
William Cameron <william@williamcameron.co.uk>
2019-10-02 22:24:03 +0100

    add stemmer tests to bring coverage up to 100%

--- test/StemmerTest.php
+++ test/StemmerTest.php
@@ -14,4 +14,42 @@ class StemmerTest extends TestCase
     public function testTrue(){
         $this->assertTrue(true);
     }
+
+    public function testMultiStemReturnsHelloWorld() {
+        $this->assertEquals(['hello', 'world'], Stemmer::multiStem("Hello World!"));
+    }
+
+    public function testMultiStemReturnsEmptyArrayForBlankString() {
+        $this->assertEquals([], Stemmer::multiStem(""));
+    }
+
+    public function testMultiStemReturnsSingularOfPlurals() {
+        $this->assertEquals(['singular', 'of', 'plural'], Stemmer::multiStem("Singulars of Plurals"));
+    }
+
+    /**
+     * @dataProvider singularPluralMap
+     */
+    public function testStemStemsAPluralWordToSingular($singular, $plural) {
+        $this->assertEquals($singular, Stemmer::stem($plural));
+    }
+        
+    public function testStemWithInvalidLanguagePassesWordThru() {
+        $this->assertEquals("composers", Stemmer::stem("composers", "INVALIDLANG"));
+    }
+
+
+    /**
+     * DataProvider and helper method for testing multiple
+     * singular to plural words in tests.
+     */
+    public function singularPluralMap(){
+        return [
+            ["", ""],
+            ["shoe", "shoes"],
+            ["plural", "plurals"],
+            ["window", "windows"],
+            ["item", "items"]
+        ];
+    }
 }
